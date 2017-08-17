c012c6b84d16dabee76010f065f461f5a17da752
LittleFisher <505516920@qq.com>
2017-08-17 19:38:57 +0800

    ng-zorro demo 暂时有报错，需等ng-zorro项目组修复

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonSharedModule } from "./shared/common-shared.module";
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonSharedModule,
+    NgZorroAntdModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
